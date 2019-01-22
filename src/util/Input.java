ffa6345faabb69e5625c21cf2d72fea38ca4660e
AaronBoehle <boehle.aaron@gmail.com>
2019-01-22 15:06:40 -0600

    init commit

--- src/util/Input.java
+++ src/util/Input.java
@@ -1,5 +1,6 @@
 package util;
 
+import java.security.cert.Extension;
 import java.util.Scanner;
 
 public class Input {
@@ -12,7 +13,7 @@ public class Input {
     public String getString(String prompt){
         System.out.println(prompt);
          String input = "";
-         input += scanner.next();
+         input += scanner.nextLine();
          return input.toLowerCase();
     }
     public boolean yesNo(String prompt) {
@@ -27,34 +28,58 @@ public class Input {
             return false;
         }
     }
+
     public int getInt (String prompt) {
         System.out.println(prompt);
-        int userInput = scanner.nextInt();
-        return userInput;
+        try {
+            String userInput = scanner.nextLine();
+            int userInteger = Integer.valueOf(userInput);
+            return userInteger;
+        } catch (Exception e) {
+            System.out.println(e);
+            return getInt(prompt);
+        }
     }
     public int getInt (String prompt, int min, int max) {
         System.out.printf(prompt, min, max);
-        int userInput = scanner.nextInt();
-        if (userInput >= min && userInput <= max) {
-            return userInput;
-        } else {
-            System.out.println("Sorry, that is not a valid number!");
+        try {
+            String userString = scanner.nextLine();
+            int userInteger = Integer.valueOf(userString);
+            if (userInteger >= min && userInteger <= max) {
+                return userInteger;
+            } else {
+                System.out.println("Sorry, that is not a valid number!");
+                return getInt(prompt, min, max);
+            }
+        } catch (Exception e) {
+            System.out.println("INVALID ENTRY: " + e);
             return getInt(prompt, min, max);
         }
     }
     public double getDouble (String prompt) {
         System.out.println(prompt);
-        int userInput = scanner.nextInt();
-        return userInput;
+        try {
+            String userString = scanner.nextLine();
+            double userDouble = Double.valueOf(userString);
+            return userDouble;
+        } catch (Exception e) {
+            System.out.println("INVALID ENTRY: " + e);
+            return getDouble(prompt);
+        }
     }
     public double getDouble (String prompt, int min, int max){
             System.out.printf(prompt);
-            int userInput = scanner.nextInt();
-            if (userInput >= min && userInput <= max) {
-                return userInput;
-            } else {
-                System.out.println("Sorry, that is not a valid number!");
-                return getDouble(prompt, min, max);
+            try {
+                String userInput = scanner.nextLine();
+                double userDouble = Double.valueOf(userInput);
+                if (userDouble >= min && userDouble <= max) {
+                    return userDouble;
+                } else {
+                    System.out.println("Sorry, that is not a valid number!");
+                    return getDouble(prompt, min, max);
+                }
+            } catch (Exception e) {
+                System.out.println(e);
+                return getDouble(prompt);
             }
-        }
-}
+}}
